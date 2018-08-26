aa4af66ce809b1a33783874735e2e1e505e21336
seungkyu2 <zx6658@naver.com>
2018-08-26 18:19:31 +0900

    edit style

--- pages/theater.js
+++ pages/theater.js
@@ -1,11 +1,27 @@
 import Layout from "../components/MyLayout";
 import React, { Component } from "react";
-import {inject, observer} from "mobx-react"
-import DevTools from 'mobx-react-devtools'
+import {inject, observer} from "mobx-react";
+import DevTools from 'mobx-react-devtools';
+import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  card: {
+    maxWidth: 345,
+  },
+  contentCard:{
+    maxWidth: 345,
+    marginTop: 10,
+  }
+};
 
 @inject('BaseStore') @observer
 class Theater extends Component {
+  
   constructor(props){
     super(props);
     this.state={};
@@ -22,14 +38,35 @@ class Theater extends Component {
     this.getArea().then((location)=>this.props.BaseStore.initLocation(location));
   }
   render(){
-    const {BaseStore}=this.props;
-    console.log(BaseStore);
+    const { classes } = this.props;
+    const { BaseStore }=this.props;
     return (
       <div>
-        <h4>영화관</h4>
-        {BaseStore.data.theater.map((val)=><Button variant="outlined" color="primary" >{val.title}</Button>)}
-        <p>위도: { BaseStore.data.location.latitude}</p>
-        <p>경도: {BaseStore.data.location.longitude}</p>
+         <Card className={classes.card}>
+         <CardContent>
+          <h4>영화관</h4>
+          {BaseStore.data.theater.map((val)=><Button variant="outlined" color="primary" >{val.title}</Button>)}
+          </CardContent>
+         </Card>
+       
+        <Card className={classes.contentCard}>
+          <CardContent>
+            <Typography gutterBottom variant="headline" component="h2">
+              현재 위치
+            </Typography>
+            <CardActions>
+            <Button size="small" color="primary">
+            { BaseStore.data.location.latitude}
+            </Button>
+            <Button size="small" color="primary">
+            {BaseStore.data.location.longitude}
+            </Button>
+          </CardActions>
+            <Typography component="p">
+              현재 위치기반 가까운 영화관
+            </Typography>
+          </CardContent>
+        </Card>
         <DevTools />
         <style jsx>{`
      #map{
@@ -42,4 +79,5 @@ class Theater extends Component {
   }
 }
 
-export default Theater;
+export default withStyles(styles)(Theater);
+
