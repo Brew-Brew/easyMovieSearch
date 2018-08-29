cb4a35ee09095496d68b35a6f288c3025bb6869d
seungkyu2 <zx6658@naver.com>
2018-08-29 22:56:47 +0900

    styled-jsx 제거

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ import Theater from "./theater";
 import initStore from "../store/initStore"
 import Header from "../components/Header";
 const store = initStore();
-
 export default () => (
   <Card>
     <Head>
@@ -19,25 +18,5 @@ export default () => (
       <Provider BaseStore={store}>
         <Theater/>
       </Provider>
-      <style jsx>{`
-      h1,
-      a {
-        font-family: "Arial";
-      }
-      ul {
-        padding: 0;
-      }
-      li {
-        list-style: none;
-        margin: 5px 0;
-      }
-      a {
-        text-decoration: none;
-        color: blue;
-      }
-      a:hover {
-        opacity: 0.6;
-      }
-    `}</style>
   </Card>
 );
